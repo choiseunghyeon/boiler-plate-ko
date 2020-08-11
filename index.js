d2a70621b55191618a144fb1eeb907c66a00b7dd
ket87 <ket8780@gmail.com>
2020-08-11 17:55:16 +0900

    ignore mongodb URI

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+const config = require("./config/key");
 mongoose
-  .connect(
-    "mongodb+srv://dbUser:1234@youtubeclone.dhwnu.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
+
 const app = express();
 const port = 5000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!sdfsdf");
 });
-// mongodb+srv://dbUser:1234@youtubeclone.dhwnu.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
